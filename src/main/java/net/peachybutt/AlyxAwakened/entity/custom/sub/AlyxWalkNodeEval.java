20f8504df4b5765cb99dc9d373b492d28e08dac1
Peachybutt <Peachybutt@mail.com>
2025-05-18 20:15:32 -0700

    Start

--- src/main/java/net/peachybutt/AlyxAwakened/entity/custom/sub/AlyxWalkNodeEval.java
+++ src/main/java/net/peachybutt/AlyxAwakened/entity/custom/sub/AlyxWalkNodeEval.java
@@ -3,6 +3,8 @@ package net.peachybutt.AlyxAwakened.entity.custom.sub;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.FenceBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.pathfinder.BlockPathTypes;
 import net.minecraft.world.level.pathfinder.NodeEvaluator;
@@ -17,9 +19,23 @@ public class AlyxWalkNodeEval extends WalkNodeEvaluator {
     public BlockPathTypes getBlockPathType(BlockGetter level, int x, int y, int z) {
         BlockPos pos = new BlockPos(x, y - 1, z);
         BlockState state = level.getBlockState(pos);
+        Block block = state.getBlock();
+
+        if (block instanceof FenceBlock) {
+            boolean north = state.getValue(FenceBlock.NORTH);
+            boolean south = state.getValue(FenceBlock.SOUTH);
+            boolean west = state.getValue(FenceBlock.WEST);
+            boolean east = state.getValue(FenceBlock.EAST);
+
+            // If any side is open, consider it walkable
+            if (!north || !south || !west || !east) {
+                return BlockPathTypes.WALKABLE;
+            } else {
+                return BlockPathTypes.FENCE;
+            }
+        }
 
         BlockPathTypes original = super.getBlockPathType(level, x, y, z);
-        System.out.println("BLOCK PATH IS BEING CHECKED");
 
         BlockPathTypes overridden = AlyxPathLogic.overridePathType(original, state);
         return overridden;
