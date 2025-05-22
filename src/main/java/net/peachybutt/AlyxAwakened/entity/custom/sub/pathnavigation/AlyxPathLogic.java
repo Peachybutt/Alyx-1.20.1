5cba106d5307b692c2a7dbd3cacbad65038c2ac3
Peachybutt <Peachybutt@mail.com>
2025-05-22 14:50:01 -0700

    Start

--- src/main/java/net/peachybutt/AlyxAwakened/entity/custom/sub/pathnavigation/AlyxPathLogic.java
+++ src/main/java/net/peachybutt/AlyxAwakened/entity/custom/sub/pathnavigation/AlyxPathLogic.java
@@ -1,17 +1,16 @@
 package net.peachybutt.AlyxAwakened.entity.custom.sub.pathnavigation;
 
+import net.minecraft.Util;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.pathfinder.BlockPathTypes;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 public class AlyxPathLogic { //This class is to introduce additional blocks as friendly and not so friendly.
     public static final BlockPathTypes ALYX_HAPPY = BlockPathTypes.COCOA;
     public static final BlockPathTypes ALYX_MAD = BlockPathTypes.DANGER_FIRE;
-
-    public static final BlockPathTypes PARTIAL_OPEN_NORTH = BlockPathTypes.create("PARTIAL_OPEN_NORTH", 1.0F);
-    public static final BlockPathTypes PARTIAL_OPEN_SOUTH = BlockPathTypes.create("PARTIAL_OPEN_SOUTH", 1.0F);
-    public static final BlockPathTypes PARTIAL_OPEN_EAST = BlockPathTypes.create("PARTIAL_OPEN_EAST", 1.0F);
-    public static final BlockPathTypes PARTIAL_OPEN_WEST = BlockPathTypes.create("PARTIAL_OPEN_WEST",1.0F);
 
     public static BlockPathTypes overridePathType(BlockPathTypes original, BlockState blockState) {
 
--- src/main/java/net/peachybutt/AlyxAwakened/entity/custom/sub/pathnavigation/AlyxWalkNodeEval.java
+++ src/main/java/net/peachybutt/AlyxAwakened/entity/custom/sub/pathnavigation/AlyxWalkNodeEval.java
@@ -4,8 +4,11 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.FenceBlock;
+import net.minecraft.world.level.block.WallBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.pathfinder.BlockPathTypes;
+import net.minecraft.world.level.pathfinder.PathFinder;
 import net.minecraft.world.level.pathfinder.WalkNodeEvaluator;
 
 public class AlyxWalkNodeEval extends WalkNodeEvaluator {
@@ -33,6 +36,8 @@ public class AlyxWalkNodeEval extends WalkNodeEvaluator {
     public BlockPathTypes getBlockPathType(Mob mob, BlockPos pos) {
         BlockState state = mob.level().getBlockState(pos);
         BlockPathTypes original = super.getBlockPathType(mob, pos);
+        Block block = state.getBlock();
         return AlyxPathLogic.overridePathType(original, state);
     }
+
 }
